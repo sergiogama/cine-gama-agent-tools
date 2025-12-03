f40b6a8d305b4a4747d8105986928d677d41f945
Sergio Gama <sergiogama@hotmail.com>
2025-12-02 22:12:22 -0300

    REmovido todos os API Keys

--- wxo-agent/guardium-ai-add-model.sh
+++ wxo-agent/guardium-ai-add-model.sh
@@ -21,5 +21,5 @@ orchestrate models list
 
 #curl -X POST "https://iam.cloud.ibm.com/identity/token" \
 #-H "Content-Type: application/x-www-form-urlencoded" \
-#-d "apikey=mVBOT7xOfc_BYN343cQ0Fgi7JpvxAH7nNXp9_ncDUMuE&grant_type=urn:ibm:params:oauth:grant-type:apikey"
+#-d "apikey=<API Key>&grant_type=urn:ibm:params:oauth:grant-type:apikey"
 
